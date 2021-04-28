e0272e6928ff386aab152860b1ec21815833c283
SomaBito <kirinb8@outlook.com>
2021-04-28 18:52:44 +0900

    応用プロフィールアプリ配置完了

--- app/src/main/java/app/bito/soma/profile/MainActivity.kt
+++ app/src/main/java/app/bito/soma/profile/MainActivity.kt
@@ -9,13 +9,11 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        //画像を見えなくする
-        profileImage.isVisible = false
-        profileCommentText.isVisible = false
+
+
 
         button1.setOnClickListener{
-            profileImage.isVisible = true
-            profileCommentText.isVisible = true
+
 
         }
 
